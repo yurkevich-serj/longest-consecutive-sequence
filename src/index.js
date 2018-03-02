ddea65eae7cd638e0b63f3f2eac77ec395ab8a3b
yurkevich-serj <yurkevichserj@gmail.com>
2018-03-02 20:31:46 +0300

    changed increment

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 module.exports = function longestConsecutiveLength(array) {
-  
-  array.sort((a, b) => a-b)
-  let counter = 0;;
-  let helpcounter = 1;
-  if (array.length == 0){
-  	return counter;
-  }
-  if (array.length == 1){
-  	return helpcounter;
-  }
-  for(let i = 0; i < array.length;) {
-  	let helpcounter = 1;
 
-  	if(array[i+1]- array[i] == 1)
+ array = array.sort(function (a, b) {return a-b});
+  
+ let counter = 0, helpcounter = 1;
+ if(array.length == 0){
+ 	return 0;
+ }
+ if(array.length == 1){
+ 	return 1;
+ }
+
+ for(let i = 0; i < array.length; i++) {
+  	
+  	if(array[i+1] - array[i] == 1)
   	{
   		helpcounter++;
-  		i++
-  	} else if (array[i] = array[i+1]){
-  		continue;
-  	} else {
-  		if (helpcounter > counter) {
   		
-  		counter = helpcounter;
-  		helpcounter = 1;
   	}
-  }
+  	else if (array[i+1] - array[i] == 0){
+  		continue;
+  	} 
+  	else {  
+  		if (helpcounter > counter) {
+  			counter = helpcounter;
+  		    }
+  		  helpcounter = 1;
+        }
+    
+  
+}
+return counter;
 }
 
-  return counter;
-}
+
